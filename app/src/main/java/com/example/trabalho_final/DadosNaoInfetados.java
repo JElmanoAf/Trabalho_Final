6063f13ad56fc54bc4c485999476e01a59693790
JElmanoAf <joao515515@hotmail.com>
2020-04-22 23:44:31 +0100

    Layout inicial e criação de strings

---
+++ app/src/main/java/com/example/trabalho_final/DadosNaoInfetados.java
@@ -0,0 +1,14 @@
+package com.example.trabalho_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DadosNaoInfetados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_nao_infetados);
+    }
+}
